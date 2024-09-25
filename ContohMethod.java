f49fc6e9f39677c8c22e64c7f195b7fc36528a6c
Gabriel Blessing Christian Hutagaol <2381002@unai.edu>
2024-09-25 20:15:28 +0700

    Handson4

--- ContohMethod.java
+++ ContohMethod.java
@@ -1,38 +1,13 @@
 public class ContohMethod {
-    public class DenganMethod {
-        public static void main(String[] args) {
-            int gajiPokok = 5000000;
-            int tunjanganTransport = 500000;
-            int tunjanganMakan = 300000;
-            int bonus = 1000000;
 
-            int totalPenghasilan = hitungTotalPenghasilan(gajiPokok, tunjanganTransport, tunjanganMakan, bonus);
-            int pajak = hitungPajak(totalPenghasilan);
-            int gajiBersih = hitungGajiBersih(totalPenghasilan, pajak);
-
-            tampilkanDetailGaji(gajiPokok, tunjanganTransport, tunjanganMakan, bonus, pajak, gajiBersih);
-        }
-
-        public static int hitungTotalPenghasilan(int gajiPokok, int tunjanganTransport, int tunjanganMakan, int bonus) {
-            return gajiPokok + tunjanganTransport + tunjanganMakan + bonus;
-        }
-
-        public static int hitungPajak(int totalPenghasilan) {
-            return totalPenghasilan * 10 / 100;
-        }
-
-        public static int hitungGajiBersih(int totalPenghasilan, int pajak) {
-            return totalPenghasilan - pajak;
-        }
-
-        public static void tampilkanDetailGaji(int gajiPokok, int tunjanganTransport, int tunjanganMakan, int bonus, int pajak, int gajiBersih) {
-            System.out.println("Gaji Pokok: " + gajiPokok);
-            System.out.println("Tunjangan Transport: " + tunjanganTransport);
-            System.out.println("Tunjangan Makan: " + tunjanganMakan);
-            System.out.println("Bonus: " + bonus);
-            System.out.println("Pajak: " + pajak);
-            System.out.println("Gaji Bersih: " + gajiBersih);
-        }
+    // Method untuk menghitung luas persegi
+    public static int hitungLUasPersegi(int sisi) {
+        int luas = sisi * sisi;
+        return luas;
     }
 
+    public static void main(String[] args) {
+        int sisi = 5;
+        // TODO: part ini belum
+    }
 }
